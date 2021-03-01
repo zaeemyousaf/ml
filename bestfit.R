57f55a75a5a784077ceb6fd13bfa2f1be5a60e1e
zaeemyousaf <quaidzaeem@gmail.com>
2021-03-01 10:38:05 +0500

    linear model regression

---
+++ bestfit.R
@@ -0,0 +1,16 @@
+bestfit <-function(x,y){
+	xbar = mean(x)
+	ybar = mean(y)
+	xdif = x-xbar
+	ydif = y-ybar	
+	pro = xdif*ydif
+	num = sum(pro)
+	den = sum(xdif^2)
+	beta = num/den
+	
+	alpha= ybar-beta *xbar
+	plot(x,y,type='p')
+	abline(alpha,beta)
+}
+
+bestfit(rnorm(50)*10,rnorm(50)*10)
